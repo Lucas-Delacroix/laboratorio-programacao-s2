69679c38f058d743139a258cc3c04e04407ca3ce
Lucas Delacroix <89474583+Lucas-Delacroix@users.noreply.github.com>
2024-03-19 19:34:34 -0300

    Add files via upload

--- lista-01/questao21.c
+++ lista-01/questao21.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(){
-    p
+int main() {
+    int n1, n2;
+
+    
+    printf("Digite o primeiro inteiro: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo inteiro: ");
+    scanf("%d", &n2);
+
+    printf("%d %s múltiplo de %d.\n", n1, (n1 % n2 == 0) ? "É" : "Não é", n2);
+
+    return 0;
 }
